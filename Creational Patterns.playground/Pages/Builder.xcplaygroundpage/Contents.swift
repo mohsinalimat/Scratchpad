e247f70cbd12e96bf9785e3019d4182a56b72c92
Sebastian Boldt <self.dealloc@googlemail.com>
2016-10-30 17:54:58 +0100

    Played around with the builder pattern

--- Creational Patterns.playground/Pages/Builder.xcplaygroundpage/Contents.swift
+++ Creational Patterns.playground/Pages/Builder.xcplaygroundpage/Contents.swift
@@ -2,6 +2,46 @@
 
 import Foundation
 
-var str = "Hello, playground"
+
+class Builder {
+    
+    var x: Double?
+    var y: Double?
+    var z: Double?
+    
+    typealias BuilderClosure = (Builder) -> ()
+    
+    init(closure:BuilderClosure) {
+        closure(self)
+    }
+}
+
+struct ThingToBuild: CustomStringConvertible {
+    let x : Double
+    let y : Double
+    let z : Double
+    
+    init?(builder: Builder) {
+        if let x = builder.x, y = builder.y, z = builder.z {
+            self.x = x
+            self.y = y
+            self.z = z
+        } else {
+            return nil
+        }
+    }
+    
+    var description:String {
+        return "Death Star at (x:\(x) y:\(y) z:\(z))"
+    }
+}
+
+let testBuilder = Builder { builder in
+    builder.x = 10
+    builder.y = 20
+    builder.z = 30
+}
+
+let instance = ThingToBuild(builder: testBuilder)
 
 //: [Next](@next)
